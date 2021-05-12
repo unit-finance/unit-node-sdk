d931fcedf2cf4f1f898a13c651d2252c37bf714f
axshani <aalexshani@gmail.com>
2021-05-12 18:35:20 +0300

    applicationDocument was moved to types\application.ts

---
+++ resources/application.ts
@@ -0,0 +1,7 @@
+import {Application} from "../types/application"
+
+namespace unit {
+    namespace applications {
+        function list(): void {} //Array<Application> {}
+    }
+}
---
+++ resources/client.ts
@@ -0,0 +1,5 @@
+declare module unit {
+    namespace unit {
+
+    }
+}

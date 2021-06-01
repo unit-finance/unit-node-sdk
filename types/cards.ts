dc2ac6975874e58238036083fdca35ee9218ed65
axshani <aalexshani@gmail.com>
2021-06-01 12:50:05 +0300

    virtual cards types were created

--- types/cards.ts
+++ types/cards.ts
@@ -104,7 +104,7 @@ export interface BusinessDebitCard {
 
          /**
           * Only when Passport is populated. Two letters representing the card holder nationality. (e.g. “US”).
-          * ISO31661-Alpha2 string	
+          * ISO31661 - Alpha2 format. For more information: https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2	
           */	
          nationality?: string
 
@@ -149,6 +149,153 @@ export interface BusinessDebitCard {
      */
      relationships: {
         account: Relationship
+        customer: Relationship
+     }
+}
+
+export interface IndividualVirtualDebitCard {
+    /**
+     * Identifier of the card resource.
+     */
+    id: string
+
+    /**
+     * Type of the card resource. For Business debit card the value is always individualVirtualDebitCard.
+     */
+    type: 'individualVirtualDebitCard'
+
+    /**
+     * JSON object representing the card data.
+     */
+    attributes: {
+        /**
+         * Date only. The date the resource was created.
+         * RFC3339 format. For more information: https://en.wikipedia.org/wiki/ISO_8601#RFCs
+         */
+         createdAt: string
+         
+         /**
+          * Last 4 digits of the debit card.
+          */
+         last4Digits: string
+         
+         /**
+          * Card expiration date, formatted YYYY-MM, e.g "2020-05".
+          */
+         expirationDate: string
+
+         /**
+          * Status of the card, one of: Active, Inactive, Stolen, Lost, Frozen, ClosedByCustomer, SuspectedFraud.
+          */
+         status: 'Active' | 'Inactive' | 'Stolen' | 'Lost' | 'Frozen' | 'ClosedByCustomer' | 'SuspectedFraud'
+    }
+
+    /**
+     * Describes relationships between the card resource and other resources (account and customer).
+     */
+     relationships: {
+         /**
+          * The account the card belongs to.
+          */
+        account: Relationship
+
+        /**
+         * The individual or business customer the card belongs to.
+         */
+        customer: Relationship
+     }
+}
+
+export interface BusinessVirtualDebitCard {
+    /**
+     * Identifier of the card resource.
+     */
+    id: string
+
+    /**
+     * Type of the card resource. For Business debit card the value is always businessVirtualDebitCard.
+     */
+    type: 'businessVirtualDebitCard'
+
+    /**
+     * JSON object representing the card data.
+     */
+    attributes: {
+        /**
+         * Date only. The date the resource was created.
+         * RFC3339 format. For more information: https://en.wikipedia.org/wiki/ISO_8601#RFCs
+         */
+         createdAt: string
+         
+         /**
+          * Last 4 digits of the debit card.
+          */
+         last4Digits: string
+         
+         /**
+          * Card expiration date, formatted YYYY-MM, e.g "2020-05".
+          */
+         expirationDate: string
+
+         /**
+          * SSN of the card holder (numbers only). Either an SSN or a passport number is required.
+          */
+         ssn?: string
+
+         /**
+          * Passport number of the card holder. Either an SSN or a passport is required.
+          */
+         passport?: string
+
+         /**
+          * Only when Passport is populated. Two letters representing the card holder nationality. (e.g. “US”).
+          * ISO31661 - Alpha2 format. For more information: https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
+          */	
+         nationality?: string
+
+         /**
+          * Full name of the card holder.
+          */
+         fullName: FullName
+
+         /**
+          * RFC3339 Date string	Date of birth of the card holder (e.g. "2001-08-15").
+          */
+         dateOfBirth: string
+
+         /**
+          * Address of the card holder.
+          */
+         address: Address
+         
+         /**
+          * Phone of the card holder.
+          */
+         phone: Phone
+
+         /**
+          * Email address of the card holder.
+          */	
+         email: string
+
+         /**
+          * Status of the card, one of: Active, Inactive, Stolen, Lost, Frozen, ClosedByCustomer, SuspectedFraud.
+          */
+         status: 'Active' | 'Inactive' | 'Stolen' | 'Lost' | 'Frozen' | 'ClosedByCustomer' | 'SuspectedFraud'
+    }
+
+    /**
+     * Describes relationships between the card resource and other resources (account and customer).
+     */
+     relationships: {
+         /**
+          * Account the card belong to.
+          */
+        account: Relationship
+
+        /**
+         * Holder of the account.
+         */
         customer: Relationship
      }
 }

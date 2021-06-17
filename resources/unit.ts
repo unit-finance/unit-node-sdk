4382dd04774e236b020441a0fa46a58914380d6d
axshani <aalexshani@gmail.com>
2021-06-17 17:46:06 +0300

    addded transactions to unit

--- resources/unit.ts
+++ resources/unit.ts
@@ -1,13 +1,16 @@
 import { Applications } from './application'
 import { Customers } from './customer';
+import { Transactions } from './transactions';
 
 export class Unit {
     public applications: Applications
     public customers: Customers
+    public transactions: Transactions
 
     constructor(token: string, basePath: string) {
         this.applications = new Applications(token, basePath);
         this.customers = new Customers(token, basePath)
+        this.transactions = new Transactions(token,basePath)
     }
 }
 

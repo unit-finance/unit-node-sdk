327d855e37c9fb263d5c349812f1fe276c7c9783
axshani <aalexshani@gmail.com>
2021-06-22 21:26:06 +0300

    first commit

--- resources/customerToken.ts
+++ resources/customerToken.ts
@@ -1,5 +1,18 @@
+import { UnitError, UnitResponse } from "../types/core";
+import { CreateTokenRequest, CustomerToken, CreateTokenVerificationRequest, VerificationToken } from "../types/customerToken";
 import { BaseResource } from "./baseResource";
 
 export class Customers extends BaseResource {
-    
+    constructor(token: string,url: string){
+        super(token,'')
+    }
+
+    public async createToken(request: CreateTokenRequest) : Promise<UnitResponse<CustomerToken> | UnitError> {
+        return this.httpPost<UnitResponse<CustomerToken> | UnitError>(`customers/${request.customerId}/token`, { data: request.data })
+    }
+
+    public async createTokenVerification(request: CreateTokenVerificationRequest) : Promise<UnitResponse<VerificationToken> | UnitError> {
+        return this.httpPost<UnitResponse<VerificationToken> | UnitError>(`customers/${request.customerId}/token/verification`,{ data: request.data})
+    }
 }
+

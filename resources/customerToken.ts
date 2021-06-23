0b9de3e50360a705c80b4d3d488f621f97719b81
axshani <aalexshani@gmail.com>
2021-06-23 09:54:19 +0300

    add to unit

--- resources/customerToken.ts
+++ resources/customerToken.ts
@@ -2,17 +2,18 @@ import { UnitError, UnitResponse } from "../types/core";
 import { CreateTokenRequest, CustomerToken, CreateTokenVerificationRequest, VerificationToken } from "../types/customerToken";
 import { BaseResource } from "./baseResource";
 
-export class Customers extends BaseResource {
+export class CustomerTokens extends BaseResource {
     constructor(token: string,url: string){
         super(token,'')
     }
 
-    public async createToken(request: CreateTokenRequest) : Promise<UnitResponse<CustomerToken> | UnitError> {
-        return this.httpPost<UnitResponse<CustomerToken> | UnitError>(`customers/${request.customerId}/token`, { data: request.data })
+    // public async createToken(request: CreateTokenRequest) : Promise<UnitResponse<CustomerToken> | UnitError> {
+    public async createToken(request: CreateTokenRequest) {
+        return this.httpPost<UnitResponse<CustomerToken>>(`customers/${request.customerId}/token`, { data: request.data })
     }
 
     public async createTokenVerification(request: CreateTokenVerificationRequest) : Promise<UnitResponse<VerificationToken> | UnitError> {
-        return this.httpPost<UnitResponse<VerificationToken> | UnitError>(`customers/${request.customerId}/token/verification`,{ data: request.data})
+        return this.httpPost<UnitResponse<VerificationToken>>(`customers/${request.customerId}/token/verification`,{ data: request.data})
     }
 }
 

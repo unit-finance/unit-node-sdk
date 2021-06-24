63c57ab33e7c38db33dd7f242af4fa2cf9558c3a
axshani <aalexshani@gmail.com>
2021-06-24 18:14:36 +0300

    indentions

--- resources/webhooks.ts
+++ resources/webhooks.ts
@@ -6,33 +6,33 @@ export class Webhooks extends BaseResource {
     constructor(token: string, basePath: string) {
         super(token, basePath + '/webhooks')
     }
- 
-    public async create(request: CreateWebhookRequest) : Promise<UnitResponse<Webhook> | UnitError> {
-        return this.httpPost<UnitResponse<Webhook> | UnitError>("",{data: request})
+
+    public async create(request: CreateWebhookRequest): Promise<UnitResponse<Webhook> | UnitError> {
+        return this.httpPost<UnitResponse<Webhook> | UnitError>("", { data: request })
     }
- 
-    public async get(id: string) : Promise<UnitResponse<Webhook> | UnitError> {
+
+    public async get(id: string): Promise<UnitResponse<Webhook> | UnitError> {
         return this.httpGet<UnitResponse<Webhook> | UnitError>(`/${id}`)
     }
- 
-    public async list(params?: WebhookListParams) : Promise<UnitResponse<Webhook[]> | UnitError> {
+
+    public async list(params?: WebhookListParams): Promise<UnitResponse<Webhook[]> | UnitError> {
         let parameters = {
             'page[limit]': (params?.limit ? params.limit : 100),
             'page[offset]': (params?.offset ? params.offset : 0)
         }
- 
-        return this.httpGet<UnitResponse<Webhook[]> | UnitError>("", {params: parameters})
+
+        return this.httpGet<UnitResponse<Webhook[]> | UnitError>("", { params: parameters })
     }
- 
-    public async update(id:string, request: PatchWebhookRequest) : Promise<UnitResponse<Webhook> | UnitError> {
-        return this.httpPatch<UnitResponse<Webhook> | UnitError>(`/${id}`,{data: request})
+
+    public async update(id: string, request: PatchWebhookRequest): Promise<UnitResponse<Webhook> | UnitError> {
+        return this.httpPatch<UnitResponse<Webhook> | UnitError>(`/${id}`, { data: request })
     }
- 
-    public async enable(id: string) : Promise<UnitResponse<Webhook> | UnitError>{
+
+    public async enable(id: string): Promise<UnitResponse<Webhook> | UnitError> {
         return this.httpPost<UnitResponse<Webhook> | UnitError>(`/${id}/enable`)
     }
- 
-    public async disable(id: string) : Promise<UnitResponse<Webhook> | UnitError>{
+
+    public async disable(id: string): Promise<UnitResponse<Webhook> | UnitError> {
         return this.httpPost<UnitResponse<Webhook> | UnitError>(`/${id}/disable`)
     }
 }

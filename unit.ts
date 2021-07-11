ee53a079605041c93ca0f7972b7f4e6af48c3754
miguel obando <miguel@obando.com.ve>
2021-07-11 08:51:00 -0500

    feat(helpers): expose all helpers directly in class unit

--- unit.ts
+++ unit.ts
@@ -9,7 +9,7 @@ import { Webhooks } from "./resources/webhooks"
 import { UnitError } from "./types/common"
 import { BatchAccounts } from "./resources/batchAccounts"
 import { Fees } from "./resources/fee"
-import { createAddress, createFullName, createPhone, createAuthorizedUser, createBeneficialOwner, createBusinessContact, createCoordinates, createCounterparty, createOfficer } from "./helpers"
+import * as helpers from "./helpers"
 import { Counterparty } from "./resources/counterparty"
 import { Events } from "./resources/events"
 import { Payments } from "./resources/payments"
@@ -28,7 +28,7 @@ export class Unit {
     public counterparties: Counterparty
     public payments: Payments
     public authorizations: Authorizations
-    public helpers: any
+    public helpers: typeof helpers;
     public events: Events
 
     constructor(token: string, basePath: string) {
@@ -45,7 +45,7 @@ export class Unit {
         this.events = new Events(token, basePath)
         this.payments = new Payments(token, basePath)
         this.authorizations = new Authorizations(token, basePath)
-        this.helpers = { createAddress, createFullName, createPhone, createAuthorizedUser, createBeneficialOwner, createBusinessContact, createCoordinates, createCounterparty, createOfficer }
+        this.helpers = helpers;
     }
 
     isError<T>(response: T | UnitError): response is UnitError {

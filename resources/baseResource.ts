deb12ffa5c5d1dcb13d9384a2ebf5d315fb4c348
Doron Somech <somdoron@gmail.com>
2021-09-17 18:21:10 +0300

    fix upload document

--- resources/baseResource.ts
+++ resources/baseResource.ts
@@ -52,7 +52,7 @@ export class BaseResource {
             .catch(error => { throw extractUnitError(error) })
     }
 
-    protected async httpPut<T>(path: string, data: object, config?: { headers?: object; params?: object; }) : Promise<T>{
+    protected async httpPut<T>(path: string, data: object | Buffer, config?: { headers?: object; params?: object; }) : Promise<T>{
         const conf = {
             headers: this.mergeHeaders(config?.headers),
             ...(config?.params && { params: (config.params) })

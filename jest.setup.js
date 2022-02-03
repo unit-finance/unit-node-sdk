dd0fc37f62731869173ff3a4ace05aecc61fad2e
ilyamerman <ilya@unit.co>
2022-02-03 14:02:34 +0200

    wait

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,5 @@
 jest.setTimeout(60000);
 jest.retryTimes(5);
-beforeEach(async () => {
-    await setTimeout(() => {}, 5000)
+beforeEach(() => {
+    return new Promise(resolve => setTimeout(resolve, 5000));
 })

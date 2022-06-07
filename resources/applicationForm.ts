973a8f6e2446f26f51fd5f310fc2b6ec8eed356e
axshani <aalexshani@gmail.com>
2022-06-07 12:23:09 +0300

    added list to ApplicationForms with tests

--- resources/applicationForm.ts
+++ resources/applicationForm.ts
@@ -1,5 +1,5 @@
 import { BaseResource } from "./baseResource"
-import { UnitConfig, UnitResponse } from "../types/common"
+import { BaseListParams, UnitConfig, UnitResponse } from "../types/common"
 import { CreateApplicationFormRequest, CreateApplicationFormResponse, ApplicationForm } from "../types/applicationForm"
 
 export class ApplicationForms extends BaseResource {
@@ -14,4 +14,21 @@ export class ApplicationForms extends BaseResource {
     public async get(applicationFormId: string) : Promise<UnitResponse<ApplicationForm>> {
         return this.httpGet<UnitResponse<ApplicationForm>>(`/${applicationFormId}`)
     }
+    
+    public async list(params?: ApplicationFormsListParams) : Promise<UnitResponse<ApplicationForm[]>> {
+        const parameters = {
+            "page[limit]": (params?.limit ? params?.limit : 100),
+            "page[offset]": (params?.offset ? params?.offset : 0),
+            ...(params?.tags && {"filter[tags]": params?.tags}),
+        }
+        return this.httpGet<UnitResponse<ApplicationForm[]>>("", {params: parameters})
+    }
+}
+
+export interface ApplicationFormsListParams extends BaseListParams {
+    /**
+     * Optional. Filter Applications by [Tags](https://developers.unit.co/#tags).
+     * default: empty
+     */
+    tags?: object
 }

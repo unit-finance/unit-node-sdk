62fa91e9fd5d29f9790c4068983707a6074f547a
axshani <aalexshani@gmail.com>
2022-06-20 16:16:22 +0300

    fix tests

--- tests/accounts.spec.ts
+++ tests/accounts.spec.ts
@@ -1,8 +1,9 @@
 import { Unit } from "../unit"
-import { CreateDepositAccountRequest } from "../types/account"
+import { AccountOwnersRequest, CreateDepositAccountRequest } from "../types/account"
 import { createBusinessApplication, createIndividualApplication } from "./applications.spec"
 
 import dotenv from "dotenv"
+import { createRelationshipArray } from "../helpers"
 dotenv.config()
 const unit = new Unit(process.env.UNIT_TOKEN || "test", process.env.UNIT_API_URL || "test")
 const accountsId: string[] = []
@@ -59,6 +60,20 @@ describe("Create Account", () => {
     test("Create Deposit Account", async () => {
         const res = await createIndividualAccount()
         const account = await unit.accounts.get(res.data.id)
+        expect(account.data.type === "depositAccount").toBeTruthy()
+    })
+})
+
+describe("Add Account Owners", () => {
+    test("add account owners", async () => {
+        const account_id = (await createIndividualAccount()).data.id
+        const account_id1 = (await createIndividualAccount()).data.id
+        const account_id2 = (await createIndividualAccount()).data.id
+        const req: AccountOwnersRequest = {
+            accountId: account_id,
+            data: createRelationshipArray("customer", [account_id1,account_id2])
+        }
+        const account = await unit.accounts.addOwners(req)
         expect(account.data.type === "depositAccount").toBeTruthy()
     })
 })

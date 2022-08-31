394bdbce979db96b07119a39a7fd97d3b22dfb55
Alex Shani <77099403+axshani@users.noreply.github.com>
2022-08-31 18:14:26 +0300

    added reasons (#233)
    Co-authored-by: Ilya Merman <ilya@unit.co>

--- types/returns.ts
+++ types/returns.ts
@@ -1,11 +1,17 @@
 import { Relationship } from "./common"
 
+export type AchReturnReason = "InsufficientFunds" | "AccountClosed" | "NoAccount" | "InvalidAccountNumberStructure" | "UnauthorizedDebitToConsumer" |
+"ReturnedPerOdfiRequest" | "AuthorizationRevokedByCustomer" | "PaymentStopped" | "UncollectedFunds" | "CustomerAdvisesNotAuthorized" | "CheckTruncationEntryReturned" | "BranchSoldToAnotherDfi" | "RdfiNotQualifiedToParticipate" |
+"RepresentativePayeeDeceasedOrUnableToContinue" | "BeneficiaryOrBankAccountHolderDeceased" | "AccountFrozen" | "FileRecordEditCriteria" | "ImproperEffectiveEntryDate" | "AmountFieldError" | "NonTransactionAccount" | "InvalidCompanyIdentification" |
+"InvalidIndividualIdNumber" | "CreditEntryRefusedByReceiver" | "DuplicateEntry" | "AddendaError" | "MandatoryFieldError" | "TraceNumberError" | "RoutingNumberCheckDigitError" | "CorporateCustomerAdvisesNotAuthorized" | "RdfiNotParticipantInCheckTruncationProgram" | "PermissibleReturnEntry" | "RdfiNonSettlement" |
+"ReturnOfXckEntry" | "LimitedParticipationDfi" | "ReturnOfImproperDebitEntry" | "ReturnOfImproperCreditEntry" | "SourceDocumentPresentedForPayment" | "StopPaymentOnSourceDocument" | "ImproperSourceDocument"
+
 export interface ReturnReceivedAchTransactionRequest {
     transactionId: string
     data: {
         type: "returnAch"
         attributes: {
-            reason: "Unauthorized"
+            reason: AchReturnReason
         }
         relationships: {
             account: Relationship

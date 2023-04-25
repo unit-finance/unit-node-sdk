e5effa06348e634f11a27fbb3635eb482367b82c
Dennis <40006449+dev-dennis-guye@users.noreply.github.com>
2023-04-25 03:22:52 -0400

    Feat: Add Fee Reversal (#322)
    * Add Fee Reversal
    
    * Fix description type

--- resources/fee.ts
+++ resources/fee.ts
@@ -1,5 +1,5 @@
 import { UnitConfig, UnitResponse } from "../types/common"
-import { CreateFeeRequest, Fee } from "../types/fee"
+import { CreateFeeRequest, Fee, FeeReversal, FeeReversalRequest } from "../types/fee"
 import { BaseResource } from "./baseResource"
 
 export class Fees extends BaseResource {
@@ -9,5 +9,9 @@ export class Fees extends BaseResource {
 
     public async createFee(request: CreateFeeRequest): Promise<UnitResponse<Fee>> {
         return this.httpPost<UnitResponse<Fee>>("", { data: request })
+    }
+
+    public async reverse(request: FeeReversalRequest): Promise<UnitResponse<FeeReversal>>{
+        return this.httpPost<UnitResponse<FeeReversal>>("/reverse", { data: request })
     }
 }

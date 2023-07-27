72ebe65affd00a44121736f50723f068eafe89df
axshani <aalexshani@gmail.com>
2023-07-27 13:09:09 +0300

    lint-fix

--- tests/checkPayments.spec.ts
+++ tests/checkPayments.spec.ts
@@ -28,14 +28,14 @@ describe("Test Check Payments", () => {
 
             const res = (await unit.checkPayments.get(cp.id)).data
 
-            expect(cp.type).toBe(res.type);
-            expect(cp.attributes.checkNumber).toBe(res.attributes.checkNumber);
-            expect(cp.attributes.createdAt).toBe(res.attributes.createdAt);
-            expect(cp.attributes.updatedAt).toBe(res.attributes.updatedAt);
-            expect(cp.attributes.description).toBe(res.attributes.description);
-            expect(cp.attributes.additionalVerificationStatus).toBe(res.attributes.additionalVerificationStatus);
-            expect(cp.attributes.amount).toBe(res.attributes.amount);
-            expect(cp.attributes.counterpartyRoutingNumber).toBe(res.attributes.counterpartyRoutingNumber);
+            expect(cp.type).toBe(res.type)
+            expect(cp.attributes.checkNumber).toBe(res.attributes.checkNumber)
+            expect(cp.attributes.createdAt).toBe(res.attributes.createdAt)
+            expect(cp.attributes.updatedAt).toBe(res.attributes.updatedAt)
+            expect(cp.attributes.description).toBe(res.attributes.description)
+            expect(cp.attributes.additionalVerificationStatus).toBe(res.attributes.additionalVerificationStatus)
+            expect(cp.attributes.amount).toBe(res.attributes.amount)
+            expect(cp.attributes.counterpartyRoutingNumber).toBe(res.attributes.counterpartyRoutingNumber)
 
         })
     })

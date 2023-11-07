1e0e3db269479ecd59e560fdcd056ed82a8492e2
axshani <aalexshani@gmail.com>
2023-11-07 19:06:58 +0200

    update ApplicationFormPrefill

--- types/applicationForm.ts
+++ types/applicationForm.ts
@@ -1,4 +1,4 @@
-import { BeneficialOwner } from "./application"
+import { AnnualIncome, AnnualRevenue, BeneficialOwner, BusinessAnnualRevenue, BusinessVertical, CashFlow, NumberOfEmployees, Occupation, SourceOfIncome } from "./application"
 import { Address, BusinessContact, EntityType, FullName, Officer, Phone, Relationship } from "./common"
 
 export type ApplicationFormStage =
@@ -85,85 +85,158 @@ export interface ApplicationFormPrefill {
      * Optional. One of "Individual", "Business" or "SoleProprietorship".
      */
     applicationType?: "Individual" | "SoleProprietorship" | "Business"
+
     /**
      * Individual. Optional. Full name of the individual.
      */
     fullName?: FullName
+
     /**
      * Individual. Optional. SSN of the individual (numbers only). Either an SSN or a passport number is required.
      */
     ssn?: string
+
     /**
      * Individual. Optional. Passport number of the individual. Either an SSN or a passport is required.
      */
     passport?: string
+
     /**
      * Individual. Optional. ISO31661-Alpha2 Two letters representing the individual nationality. (e.g. “US”).
      */
     nationality?: string
+
     /**
      * Individual Optional. RFC3339 Date only (e.g. "2001-08-15").
      */
     dateOfBirth?: string
+
     /**
      * Individual. Optional. Email address of the individual.
      */
     email?: string
+
     /**
      * Business. Optional. Name of the business.
      */
     name?: string
+
     /**
      * Business. Optional. Two letters representing a US state.
      */
     stateOfIncorporation?: string
+
     /**
      * Business. Optional. One of LLC, Partnership, PubliclyTradedCorporation, PrivatelyHeldCorporation or NotForProfitOrganization.
      */
     entityType?: EntityType
+
     /**
      * Business. Optional. Primary contact of the business.
      */
     contact?: BusinessContact
+
     /**
      * Business. Optional. Officer representing the business
      * (must be the CEO, COO, CFO, President, BenefitsAdministrationOfficer, CIO, VP, AVP, Treasurer, Secretary, Controller, Manager, Partner or Member). To onboard a business successfully,
      * you must provide the officer's personal details.
      */
     officer?: Officer
+
     /**
      * Business. Optional. Array of beneficial owners in the business.
      * Beneficial owners are all people that, directly or indirectly, own 25% or more of the business.
      * To onboard a business successfully, you must provide each beneficial owner's personal details.
      */
     beneficialOwners?: BeneficialOwner[]
+
     /**
      * Business. Optional. Business's website.
      */
     website?: string
+
     /**
      * Individual / Business. Optional. “Doing business as”.
      * If the individual is a sole proprietor who is doing business under a different name, specify it here.
      */
     dba?: string
+
     /**
      * Individual / Business. Optional.
      * If the individual is a sole proprietor who has an Employer Identification Number,
      * specify it here. / Business EIN (numbers only).
      */
     ein?: string
+
     /**
      * Individual / Business. Optional. Address of the individual / Address of the business.
      */
     address?: Address
+
     /**
      * Individual / Business. Optional. Phone number of the individual / Phone number of the business.
      */
     phone?: Phone
+
     /**
      * Individual / Business. Optional. See (this)[https://docs.unit.co/customer-api-tokens/#customers-create-customer-bearer-token-jwt] section for more information.
      */
     jwtSubject?: string
+
+    /**
+     * Optional. Occupation of the individual / sole proprietor.
+     */
+    occupation?: Occupation
+
+    /**
+     * Optional. Annual income of the individual / sole proprietor.
+     */
+    annualIncome?: AnnualIncome
+
+    /**
+     * Optional. Source of income of the individual / sole proprietor.
+     */
+    sourceOfIncome?: SourceOfIncome
+
+    /**
+     * Optional. The business vertical of the sole proprietor / business.
+     */
+    businessVertical?: BusinessVertical
+
+    /**
+     * Optional. Annual revenue of the sole proprietor / business.
+     */
+    annualRevenue?: AnnualRevenue
+
+    /**
+     * Optional. Number of employees of the sole proprietor / business.
+     */
+    numberOfEmployees?: NumberOfEmployees
+
+    /**
+     * Optional. Cash flow of the business.
+     */
+    cashFlow?: CashFlow
+
+    /**
+     * Optional. Year of incorporation of the business.
+     */
+    yearOfIncorporation?: string
+
+    /**
+     * Optional. An array of two letter codes representing the countries of operation of the business.
+     */
+    countriesOfOperation?: string[]
+
+    /**
+     * Optional. The stock symbol (ticker) of the business.
+     */
+    stockSymbol?: string
+
+    /**
+     * 	Optional. Indicates if any of the officer / BeneficialOwner of the business have a non US nationality.
+     */
+    hasNonUsEntities?: boolean
 }
 
 export interface ApplicationFormSettingsOverride {

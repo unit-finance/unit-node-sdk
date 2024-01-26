988499e57f9a30b7cb20e4ecaac736981e74c60d
axshani <aalexshani@gmail.com>
2024-01-26 21:37:20 +0200

    remove console.log

--- tests/atmLocations.spec.ts
+++ tests/atmLocations.spec.ts
@@ -7,9 +7,8 @@ const unit = new Unit(process.env.UNIT_TOKEN || "test", process.env.UNIT_API_URL
 describe("Get ATM Locations", () => {
     test("Get ATM Locations by coordinates",async () => {
         const res = await unit.atmLocations.list({coordinates: {longitude: -73.93041, latitude: 42.79894}})
-        
+
         res.data.forEach(element => {
-            console.log(element)
             expect(element.type).toBe("atmLocation")
         })
     })
